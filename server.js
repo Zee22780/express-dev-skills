71aa5ed5497f79a92a6e90a9674eb9f6987e5414
zuri227 <zuri22780@gmail.com>
2022-02-26 20:28:22 -0500

    Add form html and form styling css

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 import * as skillsDb from "./data/skills-db.js"
+import methodOverride from 'method-override'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
@@ -28,6 +29,7 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
+app.use(methodOverride("_method"))
 
 // mounted routers
 app.use('/', indexRouter)
